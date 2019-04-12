c5c3526e43e9f98b7571329261b5e8c56a0d0181
Blue <realchangyu@gmail.com>
2019-04-12 17:36:48 +0800

    update

--- 剑指Offer/9-用两个栈实现队列.py
+++ 剑指Offer/9-用两个栈实现队列.py
@@ -1,8 +1,6 @@
 '''
-题目1：用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+题目：用两个栈来实现一个队列，完成队列的Push、Pop、Top操作。
 '''
-
-
 class MyQueue:
 
     def __init__(self):
@@ -48,3 +46,31 @@ class MyQueue:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
             return self.stack2.pop()
+
+'''
+题目：用两个队列实现一个栈，完成栈的Push、Pop操作。
+'''
+class QueueToStack:
+    def __init__(self):
+        self.queue1 = []
+        self.queue2 = []
+
+    def push(self, element):
+        if self.queue2:
+            self.queue2.append(element)
+        else:
+            self.queue1.append(element)
+
+
+    def pop(self):
+        if not self.queue1 and not self.queue2:
+            return None
+
+        if self.queue1:
+            while len(self.queue1) > 1:
+                self.queue2.append(self.queue1.pop(0))
+            return self.queue1.pop()
+        else:
+            while len(self.queue2) > 1:
+                self.queue1.append(self.queue2.pop(0))
+            return self.queue2.pop()
