83569e973f7bb65fc2e26d5ab44b453891ddc3cb
Blue <realchangyu@gmail.com>
2019-04-12 09:58:10 +0800

    update

---
+++ 剑指Offer/6-从尾到头打印链表.py
@@ -0,0 +1,30 @@
+'''
+题目：
+输入一个链表的头节点，从尾到头反过来打印出每个节点的值。
+'''
+
+#Definition of ListNode
+
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+#递归
+def PrintListReversingly(head):
+    if head:
+        if head.next:
+            PrintListReversingly(head.next)
+        print(head.val)
+
+#栈
+def PrintListReversingly2(head):
+    stack = []
+    while head != None:
+        stack.append(head.val)
+        head = head.next
+    while stack:
+        print(stack.pop())
+    print()
+
