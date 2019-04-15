0e9a200bae266b73be074e9723995c7e9b82c068
Blue <realchangyu@gmail.com>
2019-04-15 10:38:59 +0800

    update

---
+++ 剑指Offer/10-斐波那契数列.py
@@ -0,0 +1,16 @@
+'''
+题目一：求斐波那契数列的第n项
+'''
+class Solution:
+    """
+    @param n: an integer
+    @return: an ineger f(n)
+    """
+    def fibonacci(self, n):
+        # write your code here
+        result = [0, 1]
+        for i in range(2,n+1):
+            result.append(result[i-1]+result[i-2])
+        return result[n]
+
+
