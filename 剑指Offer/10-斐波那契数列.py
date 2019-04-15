181e1056e98cd9a2914daf6bf1fddf9ee278960f
Blue <realchangyu@gmail.com>
2019-04-15 15:00:03 +0800

    update

--- 剑指Offer/10-斐波那契数列.py
+++ 剑指Offer/10-斐波那契数列.py
@@ -11,14 +11,24 @@ class Solution:
         result = [0, 1]
         for i in range(2,n+1):
             result.append(result[i-1]+result[i-2])
-        return result[n]
+            return result[n]
 
 #最优写法 时间O（logn）
 import numpy as np
 def Fibonacci_Matrix(n):
-    result_list = []
     Matrix = np.matrix('1 1;1 0')
-    for i in range(0, n):
-        result_list.append(np.array(pow(Matrix, i))[0][0])
-    return result_list
+    if n == 1:
+        return Matrix
+    if n == 2:
+        return pow(Matrix, 2)
+    elif n % 2 == 1:
+        return pow(Fibonacci_Matrix((n - 1) / 2),2) * Matrix
+    else:
+        return pow(Fibonacci_Matrix(n / 2),2)
 
+'''
+题目二：青蛙跳台阶问题
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+解法：
+其实就是斐波那契数列
+'''
