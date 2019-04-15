5f6309612c53edf305f9a1a263c6dba9cc1b51fe
Blue <realchangyu@gmail.com>
2019-04-15 10:39:09 +0800

    update

--- 剑指Offer/10-斐波那契数列.py
+++ 剑指Offer/10-斐波那契数列.py
@@ -13,4 +13,12 @@ class Solution:
             result.append(result[i-1]+result[i-2])
         return result[n]
 
+#最优写法 时间O（logn）
+import numpy as np
+def Fibonacci_Matrix(n):
+    result_list = []
+    Matrix = np.matrix('1 1;1 0')
+    for i in range(0, n):
+        result_list.append(np.array(pow(Matrix, i))[0][0])
+    return result_list
 
