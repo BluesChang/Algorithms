72b94ceabb763440ba5a2f612cf2d86ed2f46db0
Blue <realchangyu@gmail.com>
2019-04-25 16:33:16 +0800

    update

---
+++ 剑指Offer/32-1-不分行从上到下打印二叉树.py
@@ -0,0 +1,18 @@
+'''
+题目：从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+'''
+
+
+class Solution:
+    def print_from_top_to_bottom(self, root):
+        if not root:
+            return
+        queue = []
+        queue.append(root)
+        while queue:
+            node = queue.pop(0)
+            print(node.val)
+            if root.left:
+                queue.append(node.left)
+            if root.right:
+                queue.append(node.right)
