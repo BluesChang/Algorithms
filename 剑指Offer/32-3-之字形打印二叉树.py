3600b67b188df92ab582aa10e1031cec86a753ad
Blue <realchangyu@gmail.com>
2019-04-26 09:32:35 +0800

    update

--- 剑指Offer/32-3-之字形打印二叉树.py
+++ 剑指Offer/32-3-之字形打印二叉树.py
@@ -2,5 +2,27 @@
 题目：请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二行按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此
 类推。
 '''
+from collections import deque
+
+
 class Solution:
-    def print_from_top_to_bottom(self,root):
+    def print_from_top_to_bottom(self, root):
+        if not root:
+            return []
+        queue = deque([root])
+        result = []
+        is_from_left_to_right = True
+        while queue:
+            next_queue = []
+            if is_from_left_to_right:
+                result = [node.val for node in queue]
+            else:
+                result = [node.val for node in reversed(queue)]
+            is_from_left_to_right = not is_from_left_to_right
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue
+        print(result)
