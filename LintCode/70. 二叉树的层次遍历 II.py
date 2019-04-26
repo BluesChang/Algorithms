e52d1a911d23d3329b4cd57227834d46a3ff2443
Blue <realchangyu@gmail.com>
2019-04-26 09:40:28 +0800

    update

---
+++ LintCode/70. 二叉树的层次遍历 II.py
@@ -0,0 +1,29 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def levelOrderBottom(self, root):
+        # write your code here
+        if not root:
+            return []
+        queue = [root]
+        result = []
+        while queue:
+            next_queue = []
+            result.append([node.val for node in queue])
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue
+        return list(reversed(result))
