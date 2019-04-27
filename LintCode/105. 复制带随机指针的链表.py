c8933f9ca2b0eb24c10d25e943ae0c337578b773
Blue <realchangyu@gmail.com>
2019-04-27 12:29:04 +0800

    update

---
+++ LintCode/105. 复制带随机指针的链表.py
@@ -0,0 +1,37 @@
+"""
+Definition for singly-linked list with a random pointer.
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+"""
+
+
+class Solution:
+    # @param head: A RandomListNode
+    # @return: A RandomListNode
+    def copyRandomList(self, head):
+        # write your code here
+        if head is None:
+            return None
+        cloned_map = {}
+        cloned_head = RandomListNode(head.label)
+        cloned_map[head] = cloned_head
+        node = head
+        cloned_node = cloned_head
+        while node:
+            cloned_node.random = node.random
+            if node.next:
+                cloned_node.next = RandomListNode(node.next.label)
+                cloned_map[node.next] = cloned_node.next
+            else:
+                cloned_node.next = None
+            node = node.next
+            cloned_node = cloned_node.next
+        node = cloned_head
+        while node:
+            if node.random:
+                node.random = cloned_map[node.random]
+            node = node.next
+        return cloned_head
