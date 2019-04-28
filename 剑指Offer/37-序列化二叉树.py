8a931736ef846bae29467d4852c83cc87174c972
Blue <realchangyu@gmail.com>
2019-04-28 16:37:19 +0800

    update

--- 剑指Offer/37-序列化二叉树.py
+++ 剑指Offer/37-序列化二叉树.py
@@ -1,3 +1,28 @@
 '''
 题目：请实现两个函数，分别用来序列化和反序列化二叉树。
 '''
+
+
+class Solution:
+    def serialize(self, root):
+        if not root:
+            print('$,', end='')
+            return
+        print(root.val, end='')
+        self.serialize(root.left)
+        self.serialize(root.right)
+
+    def deserialize(self, serialize):
+        if serialize:
+            serialize = iter(serialize.split(','))
+
+        def spanning_tree():
+            data = next(serialize)
+            if data ='$':
+                return None
+            node = TreeNode(data)
+            node.left = spanning_tree()
+            node.right = spanning_tree()
+            return node
+
+        return spanning_tree()
