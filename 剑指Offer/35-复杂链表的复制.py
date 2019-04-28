6ece9a2c430a84057ed1c252f3cec237b1278705
Blue <realchangyu@gmail.com>
2019-04-28 10:10:56 +0800

    update

--- 剑指Offer/35-复杂链表的复制.py
+++ 剑指Offer/35-复杂链表的复制.py
@@ -2,6 +2,7 @@
 题目：复制一个复杂链表。在复杂链表中，每个节点除了有一个指针指向下一个节点，还有一个指针指向链表中的任一节点或空节点。
 '''
 
+
 # 时间O(n)
 class Solution:
     def clone(self, head):
@@ -14,25 +15,28 @@ class Solution:
     def clone_nodes(self, head):
         while head:
             cloned_node = head
-                head, head.next = cloned_node.next, cloned_node
+            head, head.next = cloned_node.next, cloned_node
 
-    def connect_sibling_nodes(self, head):
-        while head:
-            cloned_node = head.next
-            if head.random:
-                cloned_node.random = head.random.next
-            head = cloned_node.next
 
-    def reconnect_nodes(self, head):
-        cloned_head = cloned_node = head.next
+def connect_sibling_nodes(self, head):
+    while head:
+        cloned_node = head.next
+        if head.random:
+            cloned_node.random = head.random.next
+        head = cloned_node.next
+
+
+def reconnect_nodes(self, head):
+    cloned_head = cloned_node = head.next
+    head.next = cloned_node.next
+    head = head.next
+    while head:
+        cloned_node.next = head.next
+        cloned_node = cloned_node.next
         head.next = cloned_node.next
         head = head.next
-        while head:
-            cloned_node.next = head.next
-            cloned_node = cloned_node.next
-            head.next = cloned_node.next
-            head = head.next
-        return cloned_head
+    return cloned_head
+
 
 # 用map辅助,空间O(n) 时间O(n)
 class Solution2:
