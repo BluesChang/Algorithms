01ae9bcb70cfb72ea613ae27b755713b8ab8836a
Blue <realchangyu@gmail.com>
2019-04-28 10:10:35 +0800

    update

--- 剑指Offer/35-复杂链表的复制.py
+++ 剑指Offer/35-复杂链表的复制.py
@@ -2,7 +2,7 @@
 题目：复制一个复杂链表。在复杂链表中，每个节点除了有一个指针指向下一个节点，还有一个指针指向链表中的任一节点或空节点。
 '''
 
-
+# 时间O(n)
 class Solution:
     def clone(self, head):
         if not head:
@@ -14,7 +14,7 @@ class Solution:
     def clone_nodes(self, head):
         while head:
             cloned_node = head
-            head, head.next = cloned_node.next, cloned_node
+                head, head.next = cloned_node.next, cloned_node
 
     def connect_sibling_nodes(self, head):
         while head:
@@ -32,4 +32,33 @@ class Solution:
             cloned_node = cloned_node.next
             head.next = cloned_node.next
             head = head.next
+        return cloned_head
+
+# 用map辅助,空间O(n) 时间O(n)
+class Solution2:
+    # @param head: A RandomListNode
+    # @return: A RandomListNode
+    def copyRandomList(self, head):
+        # write your code here
+        if head is None:
+            return None
+        cloned_map = {}
+        cloned_head = RandomListNode(head.label)
+        cloned_map[head] = cloned_head
+        node = head
+        cloned_node = cloned_head
+        while node is not None:
+            cloned_node.random = node.random
+            if node.next is not None:
+                cloned_node.next = RandomListNode(node.next.label)
+                cloned_map[node.next] = cloned_node.next
+            else:
+                cloned_node.next = None
+            node = node.next
+            cloned_node = cloned_node.next
+        node = cloned_head
+        while node is not None:
+            if node.random:
+                node.random = cloned_map[node.random]
+            node = node.next
         return cloned_head
