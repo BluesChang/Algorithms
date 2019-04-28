8dd77ef63b471b149b48b5e0710b3a1734ecc76b
Blue <realchangyu@gmail.com>
2019-04-28 09:46:19 +0800

    update

--- LintCode/105. 复制带随机指针的链表.py
+++ LintCode/105. 复制带随机指针的链表.py
@@ -15,15 +15,23 @@ class Solution:
         # write your code here
         if head is None:
             return None
+        cloned_map = {}
         cloned_head = RandomListNode(head.label)
+        cloned_map[head] = cloned_head
         node = head
         cloned_node = cloned_head
         while node is not None:
             cloned_node.random = node.random
             if node.next is not None:
                 cloned_node.next = RandomListNode(node.next.label)
+                cloned_map[node.next] = cloned_node.next
             else:
                 cloned_node.next = None
             node = node.next
             cloned_node = cloned_node.next
+        node = cloned_head
+        while node is not None:
+            if node.random:
+                node.random = cloned_map[node.random]
+            node = node.next
         return cloned_head
