acf797dbf2e544bc23cc78d32e503c0fff13d0ac
Blue <realchangyu@gmail.com>
2019-05-10 11:44:47 +0800

    update

---
+++ 剑指Offer/48-最长不含重复字符的子字符串.py
@@ -0,0 +1,6 @@
+'''
+题目：请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。假设字符串中只包含‘a’~‘z’的字符。例如，在字符串”arabcacfr“中，
+最长的不含重复字符的子字符串是“acfr”，长度为4。
+'''
+class Solution:
+    def longest_substring_without_duplication(self,string):
