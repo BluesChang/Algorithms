ac2fa32f77c829e4c2e6d49e57afdcb5aa36c8fc
Blue <realchangyu@gmail.com>
2019-05-10 16:15:54 +0800

    update

--- LintCode/4. 丑数 II.py
+++ LintCode/4. 丑数 II.py
@@ -9,13 +9,19 @@ class Solution:
 
     def nthUglyNumber(self, n):
         # write your code here
-        heap = [1]
-        ugly_number = set([1])
-        val = None
-        for i in range(n):
-            val = heapq.heappop(heap)
-            for multi in [2, 3, 5]:
-                if val * multi not in ugly_number:
-                    ugly_number.add(val * multi)
-                    heapq.heappush(heap, val * multi)
-        return val
+        if not n:
+            return
+        ugly_number = [1] * n
+        next_ugly_index = 1
+        multiply2 = multiply3 = multiply5 = 0
+        while next_ugly_index < n:
+            ugly_number[next_ugly_index] = min(ugly_number[multiply2] * 2, ugly_number[multiply3] * 3,
+                                               ugly_number[multiply5] * 5)
+            while ugly_number[multiply2] * 2 <= ugly_number[next_ugly_index]:
+                multiply2 += 1
+            while ugly_number[multiply3] * 3 <= ugly_number[next_ugly_index]:
+                multiply3 += 1
+            while ugly_number[multiply5] * 5 <= ugly_number[next_ugly_index]:
+                multiply5 += 1
+            next_ugly_index += 1
+        return ugly_number[next_ugly_index - 1]
