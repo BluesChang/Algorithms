c0cb4ea9e62a82aa4b6cf7cd70873036ceffcfa1
Blue <realchangyu@gmail.com>
2019-05-13 15:35:39 +0800

    update

--- LintCode/532. 逆序对.py
+++ LintCode/532. 逆序对.py
@@ -6,35 +6,37 @@ class Solution:
 
     def reversePairs(self, A):
         # write your code here
-        self.tmp = [0] * len(A)
-        return self.mergeSort(A, 0, len(A) - 1)
+        self.copy = [0] * len(A)
+        return self.inverse_pairs_core(A, 0, len(A) - 1)
 
-    def mergeSort(self, A, l, r):
+    def inverse_pairs_core(self, data, l, r):
         if l >= r:
             return 0
 
-        m = (l + r) >> 1
-        ans = self.mergeSort(A, l, m) + self.mergeSort(A, m + 1, r)
-        i, j, k = l, m + 1, l
-        while i <= m and j <= r:
-            if A[i] > A[j]:
-                self.tmp[k] = A[j]
+        middle = (l + r) >> 1
+        result = self.inverse_pairs_core(data, l, middle) + self.inverse_pairs_core(data, middle + 1, r)
+        i, j, k = l, middle + 1, l
+
+        while i <= middle and j <= r:
+            if data[i] > data[j]:
+                self.copy[k] = data[j]
                 j += 1
-                ans += m - i + 1
+                result += middle - i + 1
             else:
-                self.tmp[k] = A[i]
+                self.copy[k] = data[i]
                 i += 1
             k += 1
 
-        while i <= m:
-            self.tmp[k] = A[i]
-            k += 1
+        while i <= middle:
+            self.copy[k] = data[i]
             i += 1
-        while j <= r:
-            self.tmp[k] = A[j]
             k += 1
-            j += 1
-        for i in range(l, r + 1):
-            A[i] = self.tmp[i]
 
-        return ans
+        while j <= r:
+            self.copy[k] = data[j]
+            j += 1
+            k += 1
+        for i in range(l, r + 1):
+            data[i] = self.copy[i]
+
+        return result
