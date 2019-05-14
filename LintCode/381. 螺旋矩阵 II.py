bab141c44adc9fe006c75eab3c53af4cd0d6c917
Blue <realchangyu@gmail.com>
2019-05-14 09:37:46 +0800

    update

--- LintCode/381. 螺旋矩阵 II.py
+++ LintCode/381. 螺旋矩阵 II.py
@@ -8,16 +8,16 @@ class Solution:
         # write your code here
         if n == 0: return []
         matrix = [[0 for i in range(n)] for j in range(n)]
-        up = 0;
+        up = 0
         down = n - 1
-        left = 0;
+        left = 0
         right = n - 1
-        direct = 0;
+        direct = 0
         count = 0
         while True:
             if direct == 0:
                 for i in range(left, right + 1):
-                    count += 1;
+                    count += 1
                     matrix[up][i] = count
                 up += 1
             if direct == 1:
@@ -27,13 +27,14 @@ class Solution:
                 right -= 1
             if direct == 2:
                 for i in range(right, left - 1, -1):
-                    count += 1;
+                    count += 1
                     matrix[down][i] = count
                 down -= 1
             if direct == 3:
                 for i in range(down, up - 1, -1):
-                    count += 1;
+                    count += 1
                     matrix[i][left] = count
                 left += 1
-            if count == n * n: return matrix
+            if count == n * n:
+                return matrix
             direct = (direct + 1) % 4
