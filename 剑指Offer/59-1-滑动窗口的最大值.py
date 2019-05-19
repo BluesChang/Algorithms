b4b681f39512216877385a7094f0ef2692e774ac
Blue <realchangyu@gmail.com>
2019-05-19 11:29:09 +0800

    update 59-1-滑动窗口的最大值

---
+++ 剑指Offer/59-1-滑动窗口的最大值.py
@@ -0,0 +1,41 @@
+'''
+题目：给定一个数组和滑动窗口的大小，请找出所有滑动窗口里的最大值。例如，如果输入数组{2, 3, 4, 2, 6, 2, 5, 1}及滑动窗口的大小3，
+那么一共存在6个滑动窗口，它们的最大值分别为{4, 4, 6, 6, 6, 5}，
+'''
+
+'''
+方法一：蛮力法
+'''
+
+
+class Solution:
+    def max_in_windows(self, nums, size):
+        if not nums or not size or size > len(nums):
+            return
+        return [max(nums[i:i + size]) for i in range(len(nums) - size + 1)]
+
+
+'''
+方法二：双向队列
+'''
+import collections
+
+
+class Solution2:
+    def max_in_windows(self, nums, size):
+        max_in_windows = []
+        if nums and 1 <= size <= len(nums):
+            index = collections.deque()
+            for i in range(size):
+                if index and nums[i] >= nums[index[0]]:
+                    index.clear()
+                index.append(i)
+            for i in range(size, len(nums)):
+                max_in_windows.append(nums[index[0]])
+                while index and nums[i] >= nums[index[len(index) - 1]]:
+                    index.pop()
+                if index and index[0] <= i - size:
+                    index.popleft()
+                index.append(i)
+            max_in_windows.append(nums[index.popleft()])
+        return max_in_windows
