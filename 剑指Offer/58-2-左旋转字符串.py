cd7feb17dd6ed326b596a7f41aa19772a1c1592b
Blue <realchangyu@gmail.com>
2019-05-20 09:55:13 +0800

    update 58-2-左旋转字符串

--- 剑指Offer/58-2-左旋转字符串.py
+++ 剑指Offer/58-2-左旋转字符串.py
@@ -3,9 +3,41 @@
 数字2，该函数将返回左旋转2位得到的结果"cdefgab"。
 '''
 
+'''
+python式方法
+'''
+
 
 class Solution:
     def left_rotate_string(self, s, n):
         if len(s) > 0:
             n %= len(s)
         return (s + s)[n:n + len(s)]
+
+
+'''
+书中方法
+'''
+
+
+class Solution2:
+    def reverse(self, data, start, end):
+        while start < end:
+            data[start], data[end] = data[end], data[start]
+            start += 1
+            end -= 1
+
+    def left_rotate_string(self, data, n):
+        if not data:
+            return
+        if len(data) > 0 and n < len(data) and n > 0:
+            firstStart = 0
+            firstEnd = n - 1
+            secondStart = n
+            secondEnd = len(data) - 1
+
+            data = list(data)
+            self.reverse(data, firstStart, firstEnd)
+            self.reverse(data, secondStart, secondEnd)
+            self.reverse(data, firstStart, secondEnd)
+            return ''.join(data)
