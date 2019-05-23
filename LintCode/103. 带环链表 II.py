562983afd440a7e61ae353cc47d79ae7f95a174e
Blue <realchangyu@gmail.com>
2019-05-23 16:34:31 +0800

    add 103. 带环链表 II

---
+++ LintCode/103. 带环链表 II.py
@@ -0,0 +1,15 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @return: The node where the cycle begins. if there is no cycle, return null
+    """
+    def detectCycle(self, head):
+        # write your code here
