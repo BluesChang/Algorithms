882294aee669e989bdd40752ae5b81ff272e6c79
Blue <realchangyu@gmail.com>
2019-05-24 14:49:08 +0800

    update 81.数据流中位数

--- LintCode/81. 数据流中位数.py
+++ LintCode/81. 数据流中位数.py
@@ -3,25 +3,27 @@ import heapq
 
 class Solution:
     """
-    @param nums: A list of integers
-    @return: the median of numbers
+    @param nums: A list of integers.
+    @return: The median of numbers
     """
 
     def medianII(self, nums):
-        # write your code here
-        if not nums:
-            return []
-        min = []
-        max = []
+        self.min = []
+        self.max = []
         medians = []
         for num in nums:
-            heapq.heappush(max, num)
-            if len(max) - len(min) > 1:
-                heapq.heappush(min, heapq.nlargest(1, max)[0])
-            if len(max) > len(min):
-                median = heapq.nlargest(1, max)[0]
-                medians.append(median)
-            else:
-                median = heapq.nlargest(1, max)[0]
-                medians.append(median)
+            self.add(num)
+            medians.append(-self.max[0])
         return medians
+
+    def add(self, value):
+        if len(self.max) <= len(self.min):
+            heapq.heappush(self.max, -value)
+        else:
+            heapq.heappush(self.min, value)
+
+        if len(self.min) == 0 or len(self.max) == 0:
+            return
+        if -self.max[0] > self.min[0]:
+            heapq.heappush(self.max, -heapq.heappop(self.min))
+            heapq.heappush(self.min, -heapq.heappop(self.max))
