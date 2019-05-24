5597df7fb2ae8c46418c6a0cfe5acb36afc85e83
Blue <realchangyu@gmail.com>
2019-05-24 15:32:15 +0800

    update 103. 带环链表 II

--- LintCode/103. 带环链表 II.py
+++ LintCode/103. 带环链表 II.py
@@ -6,10 +6,26 @@ class ListNode(object):
         self.next = next
 """
 
+
 class Solution:
     """
     @param head: The first node of linked list.
     @return: The node where the cycle begins. if there is no cycle, return null
     """
+
     def detectCycle(self, head):
         # write your code here
+        if head is None or head.next is None:
+            return None
+        slow_node = head.next
+        fast_node = head.next.next
+        while slow_node != fast_node:
+            if fast_node is None or fast_node.next is None or fast_node.next.next is None:
+                return None
+            slow_node = slow_node.next
+            fast_node = fast_node.next.next
+        slow_node = head
+        while slow_node != fast_node:
+            slow_node = slow_node.next
+            fast_node = fast_node.next
+        return fast_node
