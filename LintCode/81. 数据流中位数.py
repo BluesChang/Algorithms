8c10a396a8f22f3b06b9ccf11df2005f8221b756
Blue <realchangyu@gmail.com>
2019-05-24 14:48:24 +0800

    update 81.数据流中位数

--- LintCode/81. 数据流中位数.py
+++ LintCode/81. 数据流中位数.py
@@ -1,7 +1,27 @@
+import heapq
+
+
 class Solution:
     """
     @param nums: A list of integers
     @return: the median of numbers
     """
+
     def medianII(self, nums):
         # write your code here
+        if not nums:
+            return []
+        min = []
+        max = []
+        medians = []
+        for num in nums:
+            heapq.heappush(max, num)
+            if len(max) - len(min) > 1:
+                heapq.heappush(min, heapq.nlargest(1, max)[0])
+            if len(max) > len(min):
+                median = heapq.nlargest(1, max)[0]
+                medians.append(median)
+            else:
+                median = heapq.nlargest(1, max)[0]
+                medians.append(median)
+        return medians
