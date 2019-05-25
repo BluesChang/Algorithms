3f175ace03f8518619acf51260f91d7e1b678792
Blue <realchangyu@gmail.com>
2019-05-25 08:04:17 +0800

    update 192.通配符匹配

--- LintCode/192. 通配符匹配.py
+++ LintCode/192. 通配符匹配.py
@@ -6,3 +6,18 @@ class Solution:
     """
     def isMatch(self, s, p):
         # write your code here
+        n = len(s)
+        m = len(p)
+        f = [[False] * (m + 1) for i in range(n + 1)]
+        f[0][0] = True
+        if n == 0 and p.count('*') == m:
+            return True
+        for i in range(0, n + 1):
+            for j in range(0, m + 1):
+                if i > 0 and j > 0:
+                    f[i][j] |= f[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] in ['?', '*'])
+                if i > 0 and j > 0:
+                    f[i][j] |= f[i - 1][j] and p[j - 1] == '*'
+                if j > 0:
+                    f[i][j] |= f[i][j - 1] and p[j - 1] == '*'
+        return f[n][m]
