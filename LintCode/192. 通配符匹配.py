c0c68dc478a45d2696c050b7f97da9b3d91d79c1
Blue <realchangyu@gmail.com>
2019-05-26 09:19:00 +0800

    update 192.通配符匹配

--- LintCode/192. 通配符匹配.py
+++ LintCode/192. 通配符匹配.py
@@ -4,6 +4,7 @@ class Solution:
     @param p: A string includes "?" and "*"
     @return: is Match?
     """
+
     def isMatch(self, s, p):
         # write your code here
         n = len(s)
@@ -12,8 +13,8 @@ class Solution:
         f[0][0] = True
         if n == 0 and p.count('*') == m:
             return True
-        for i in range(0, n + 1):
-            for j in range(0, m + 1):
+        for i in range(n + 1):
+            for j in range(m + 1):
                 if i > 0 and j > 0:
                     f[i][j] |= f[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] in ['?', '*'])
                 if i > 0 and j > 0:
