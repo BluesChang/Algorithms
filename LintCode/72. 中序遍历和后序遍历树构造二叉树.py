f82f4375fc3d3649c8c144bf514802b59cc360f2
Blue <realchangyu@gmail.com>
2019-05-27 15:28:16 +0800

    add 72. 中序遍历和后序遍历树构造二叉树

---
+++ LintCode/72. 中序遍历和后序遍历树构造二叉树.py
@@ -0,0 +1,25 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param inorder: A list of integers that inorder traversal of a tree
+    @param postorder: A list of integers that postorder traversal of a tree
+    @return: Root of a tree
+    """
+
+    def buildTree(self, inorder, postorder):
+        # write your code here
+        if not inorder:
+            return
+        root = TreeNode(postorder[-1])
+        index = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:index], postorder[:index])
+        root.right = self.buildTree(inorder[index + 1:], postorder[index:-1])
+        return root
