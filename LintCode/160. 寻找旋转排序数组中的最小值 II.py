627336f3878801fd78392251699f0591cf3aafac
Blue <realchangyu@gmail.com>
2019-05-27 15:07:26 +0800

    add 160. 寻找旋转排序数组中的最小值 II

---
+++ LintCode/160. 寻找旋转排序数组中的最小值 II.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+
+    def findMin(self, nums):
+        # write your code here
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            elif nums[mid] < nums[end]:
+                end = mid
+            else:
+                end -= 1
+        return nums[start]
