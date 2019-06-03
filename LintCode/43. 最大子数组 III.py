f73c5e6f5ea33bd1acb467a41ed64dd696b3dd7c
Blue <realchangyu@gmail.com>
2019-06-03 11:16:19 +0800

    add 42. 最大子数组 II

--- LintCode/43. 最大子数组 III.py
+++ LintCode/43. 最大子数组 III.py
@@ -1,10 +1,13 @@
 import sys
+
+
 class Solution:
     """
     @param nums: A list of integers
     @param k: An integer denote to find k non-overlapping subarrays
     @return: An integer denote the sum of max k non-overlapping subarrays
     """
+
     def maxSubArray(self, nums, k):
         # write your code here
         n = len(nums)
