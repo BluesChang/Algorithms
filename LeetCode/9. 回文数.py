3312917b02a642a006d544982d7af5f24e33a332
Blue <realchangyu@gmail.com>
2019-08-16 17:44:37 +0800

    add 9. 回文数

---
+++ LeetCode/9. 回文数.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reverted_number = 0
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x = x // 10
+        return x == reverted_number or x == reverted_number // 10
