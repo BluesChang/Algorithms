485fd2d6202425d63242c4aa7bf64dd32607ad6f
Blue <realchangyu@gmail.com>
2019-08-21 16:44:41 +0800

    add 26. 删除排序数组中的重复项

---
+++ LeetCode/26. 删除排序数组中的重复项.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
