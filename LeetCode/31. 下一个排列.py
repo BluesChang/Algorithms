efa9e2685fab81994d4902ce2c438bdd9dcfcc79
Blue <realchangyu@gmail.com>
2019-08-21 17:14:08 +0800

    add 31. 下一个排列

---
+++ LeetCode/31. 下一个排列.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
