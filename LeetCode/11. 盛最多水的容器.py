8adc6075a9136299f5fb11cb295faa9727e2d2f8
Blue <realchangyu@gmail.com>
2019-08-21 16:01:42 +0800

    add 11. 盛最多水的容器

---
+++ LeetCode/11. 盛最多水的容器.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        max_area = 0
+        while l < r:
+            max_area = max(max_area, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
