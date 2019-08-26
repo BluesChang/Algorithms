1b69d3d4012a9e8fa41463c048e9b971a011969e
Blue <realchangyu@gmail.com>
2019-08-26 11:37:45 +0800

    add 字符串匹配

---
+++ 算法和数据结构/字符串/字符串匹配.py
@@ -0,0 +1,37 @@
+# 暴力求解
+def str_match(s, p):
+    m = len(s);
+    n = len(p)
+    for i in range(m - n + 1):
+        return True
+    return False
+
+
+# KMP算法
+def kmp_match(s, p):
+    # 部分匹配表
+    def partial_table(p):
+        prefix = set()
+        postfix = set()
+        table = [0]
+        for i in range(1, len(p)):
+            prefix.add(p[:i])
+            postfix = {p[j:i + 1] for j in range(1, i + 1)}
+            table.append(len((prefix & postfix or {''}).pop()))
+        return table
+
+    m = len(s)
+    n = len(p)
+    cur = 0
+    table = partial_table(p)
+    while cur <= m - n:
+        for i in range(n):
+            if s[i + cur] != p[i]:
+                cur += max(i - table[i - 1], 1)  # 有了部分匹配表,我们不只是单纯的1位1位往右移,可以一次移动多位
+                break
+        else:
+            return True
+    return False
+
+print(str_match("BBC ABCDAB ABCDABCDABDE", "ABCDABD"))
+print(kmp_match("BBC ABCDAB ABCDABCDABDE", "ABCDABD"))
