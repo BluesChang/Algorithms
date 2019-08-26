1531cf108dd4365a5112840f34c15a01f8105529
Blue <realchangyu@gmail.com>
2019-08-26 16:16:44 +0800

    add 冒泡排序

---
+++ 算法和数据结构/排序/冒泡排序.py
@@ -0,0 +1,6 @@
+def bubble_sort(array):
+    for i in range(len(array) - 1):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
