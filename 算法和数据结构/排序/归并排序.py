91da874909bec4f6da1ccd4cb8d2bed76401e71a
Blue <realchangyu@gmail.com>
2019-08-29 17:33:44 +0800

    add 归并排序

---
+++ 算法和数据结构/排序/归并排序.py
@@ -0,0 +1,21 @@
+def merge_sort(array):
+    def merge(left, right):
+        res = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        res += left[i:] + right[j:]
+        return res
+
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    return merge(left, right)
+
