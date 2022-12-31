95e9afb52b107654cd8f4471bb3245d8a23b5d11
freeflowthought <73615614+freeflowthought@users.noreply.github.com>
2022-12-31 18:50:39 -0500

    Update 21-调整数组顺序使奇数位于偶数前面.py
    bug fix for loop

--- 剑指Offer/21-调整数组顺序使奇数位于偶数前面.py
+++ 剑指Offer/21-调整数组顺序使奇数位于偶数前面.py
@@ -2,10 +2,11 @@
 题目：
 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
 '''
+import unittest
+
 class Solution:
     """
     @param: nums: an array of integers
-    @return: nothing
     """
     def reorder_odd_even(self, nums):
         if not isinstance(nums, list) or len(nums) == 0:
@@ -13,9 +14,18 @@ class Solution:
         begin = 0
         end = len(nums) - 1
         while begin < end:
-            while begin < end and (nums[begin] & 0x1) != 0:
+            while begin < end and (nums[begin] & 0x1) ==1:
                 begin += 1
             while begin < end and (nums[end] & 0x1) == 0:
                 end -= 1
             if begin < end:
                 nums[begin], nums[end] = nums[end], nums[begin]
+                begin += 1
+        return nums
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual([1, 3, 2, 4],Solution().reorder_odd_even([1,2,3,4]))
+
+Test().test()
